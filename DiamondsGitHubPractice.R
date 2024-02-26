ee2ec85c0aa9f559b0b2f85324fc63697f6682db
absommers175 <122899205+absommers175@users.noreply.github.com>
2024-02-26 18:27:50 -0500

    Update DiamondsGitHubPractice.R

--- DiamondsGitHubPractice.R
+++ DiamondsGitHubPractice.R
@@ -1,3 +1,6 @@
 library(tidyverse)
 library(dplyr)
 diamonds
+# 2)	In GitHub, edit the Git Hub Practice Rfile, showing code that will only output variables x,y, and z. Also, exactly 30 rows are to be printed.
+diamonds_XYZ <- diamonds %>% select(x,y,z)
+head(diamonds_XYZ, 30)
